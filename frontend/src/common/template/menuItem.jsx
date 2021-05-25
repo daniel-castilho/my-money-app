7b12a5c3e68b2cab05925a47601b55232b3b741d
Daniel Castilho <dan.castilho@gmail.com>
2021-05-25 19:10:56 -0400

    Navigation with React-Router

--- frontend/src/common/template/menuItem.jsx
+++ frontend/src/common/template/menuItem.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export default (props) => {
 	return (
-        <li>
-            <a href="{props.path}">
-                <i className={`fa fa-${props.icon}`}></i> {props.label}
-            </a>
-        </li>
-    )
+		<li>
+			<a href={props.path}>
+				<i className={`fa fa-${props.icon}`}></i>{" "}
+				<span>{props.label}</span>
+			</a>
+		</li>
+	);
 };
