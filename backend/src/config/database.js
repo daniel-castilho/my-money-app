df9d73d70111701b9ba65ecf7ab10c9fbfd985e7
Daniel Castilho <dan.castilho@gmail.com>
2021-05-25 10:52:12 -0400

    Config Database

---
+++ backend/src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose.connect("mongodb://localhost:27017/mymoney", {
+	useMongoClient: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
