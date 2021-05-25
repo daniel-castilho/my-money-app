1afc1340b93c5f67875a79d523c81877e01cc5d9
Daniel Castilho <dan.castilho@gmail.com>
2021-05-25 18:51:07 -0400

    Component Footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default (props) => {
+	return (
+		<footer className="main-footer">
+			<strong>
+				Copyright &copy; 2021
+				<a href="http://www.tyny.ca" target="_blank">
+					{" "}
+					Tyny
+				</a>
+			</strong>
+		</footer>
+	);
+};
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,12 +3,17 @@ import React from "react";
 
 import Header from "../common/template/header";
 import SideBar from "../common/template/sideBar";
+import Footer from "../common/template/footer";
 
 export default (props) => {
 	return (
 		<div className="wrapper">
 			<Header></Header>
             <SideBar></SideBar>
+            <div className="content-wrapper">
+                <h1>Conteudo</h1>
+            </div>
+            <Footer></Footer>
 		</div>
 	);
 };
