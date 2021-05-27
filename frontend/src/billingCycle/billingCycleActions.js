4fe07c191a783db2e67b013f91d2dd4950dcdb37
Daniel Castilho <dan.castilho@gmail.com>
2021-05-27 11:47:49 -0400

    Improvements in Create BillingCycle

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { toastr } from "react-redux-toastr";
+import { reset as resetForm } from "redux-form";
+import { showTabs, selectTab } from "../common/tab/tabActions";
+
 const BASE_URL = "http://localhost:3000/api";
 
 export function getList() {
@@ -11,16 +14,21 @@ export function getList() {
 }
 
 export function create(values) {
-	axios.post(`${BASE_URL}/billingCycles`, values)
-		.then((res) => {
-			toastr.success("Sucesso", "Operação Realizada com Sucesso");
-		})
-		.catch((e) => {
-			e.response.data.errors.forEach((error) =>
-				toastr.error("Erro", error)
-			);
-		});
-	return {
-		type: "BILLING_CYCLES_CREATE",
+	return (dispatch) => {
+		axios.post(`${BASE_URL}/billingCycles`, values)
+			.then((res) => {
+				toastr.success("Sucesso", "Operação Realizada com Sucesso");
+				dispatch([
+					resetForm("billingCycleForm"),
+					getList(),
+					selectTab("tabList"),
+					showTabs("tabList", "tabCreate"),
+				]);
+			})
+			.catch((e) => {
+				e.response.data.errors.forEach((error) =>
+					toastr.error("Erro", error)
+				);
+			});
 	};
 }
